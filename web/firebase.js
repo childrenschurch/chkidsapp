f2cf4be2d1c38dd292f6a8e13a16450b86f442ac
ugfire <ugfirecompany@gmail.com>
2025-09-08 16:42:38 +0300

    signin with Google not working yet

--- web/firebase.js
+++ web/firebase.js
@@ -1,27 +1,28 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getAnalytics } from "firebase/analytics";
 import { getMessaging } from "firebase/messaging";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDItO4zmbvSm6pTa_KqVWDUtVse-ZSgc2A",
-  authDomain: "chkidsapp.firebaseapp.com",
-  projectId: "chkidsapp",
-  storageBucket: "chkidsapp.firebasestorage.app",
-  messagingSenderId: "592661765920",
-  appId: "1:592661765920:web:81b6f85f424de0d354c145",
+  apiKey: "AIzaSyBp98X2ECrZE4GvfRrAIg0zwnxN2T6bWCs",
+  authDomain: "chkidsapp-471509.firebaseapp.com",
+  projectId: "chkidsapp-471509",
+  storageBucket: "chkidsapp-471509.firebasestorage.app",
+  messagingSenderId: "784800493341",
+  appId: "1:784800493341:web:171ff26f09569da1ab3bf4",
+  measurementId: "G-HJGWV4MNB5",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
-const analytics = getAnalytics(app);
 const messaging = getMessaging(app);
 
-export { app, auth, db, storage, analytics, messaging };
+export { app, analytics, auth, db, storage, messaging };
